e3c098810ca1bec7d4540a397f2285fdbe4ead38
Keiyle11 <keiyledeocampo@gmail.com>
2026-03-08 22:58:46 +0800

    Add files via upload

---
+++ Task2-/BinaryTree.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def PreOrder(self):
+        print(self.val, end=' ')
+        if self.left:
+            self.left.PreOrder()
+        if self.right:
+            self.right.PreOrder()
+
+    def InOrder(self):
+        if self.left:
+            self.left.InOrder()
+        print(self.val, end=' ')
+        if self.right:
+            self.right.InOrder()
+
+    def PostOrder(self):
+        if self.left:
+            self.left.PostOrder()
+        if self.right:
+            self.right.PostOrder()
+        print(self.val, end=' ')
+    
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.right.right = Node(5)
+root.left.left.left = Node(6)
+print("Pre order Traversal: ", end="")
+root.PreOrder()
+print("\nIn order Traversal: ", end="")
+root.InOrder()
+print("\nPost order Traversal: ", end="")
+root.PostOrder()
+
+
