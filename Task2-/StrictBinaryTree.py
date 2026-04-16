eaea4047b8039d60a59c1b91ead8fc6133e46b45
Keiyle11 <keiyledeocampo@gmail.com>
2026-04-16 17:24:09 +0800

    Update and rename BinaryTree.py to StrictBinaryTree.py

--- Task2-/StrictBinaryTree.py
+++ Task2-/StrictBinaryTree.py
@@ -24,20 +24,20 @@ class Node:
         if self.right:
             self.right.PostOrder()
         print(self.val, end=' ')
-    
-
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
-root.right.right = Node(5)
-root.left.left.left = Node(6)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+root.left.left.left = Node(8)
+root.left.left.right = Node(9)
+
 print("Pre order Traversal: ", end="")
 root.PreOrder()
 print("\nIn order Traversal: ", end="")
 root.InOrder()
 print("\nPost order Traversal: ", end="")
 root.PostOrder()
-
-
